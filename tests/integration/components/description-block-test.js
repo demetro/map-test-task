a8154319f6cd022bb17d2401493f19c4f34062da
demetro <sforeman@bk.ru>
2016-10-04 11:10:44 +1000

    Fixed integration test

--- tests/integration/components/description-block-test.js
+++ tests/integration/components/description-block-test.js
@@ -20,5 +20,5 @@ test('it renders', function(assert) {
     {{/description-block}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '');
 });
